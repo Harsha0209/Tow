8c9ad3efe8cc94513fecee3f94f22f48d13f67fd
sriharsha <sriharsha.jee@gmail.com>
2023-05-01 12:41:32 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div>
+      <a href="https://zegocloud.github.io/zego_uikit_prebuilt_web/video_conference/index.html" target="_blank" rel="noopener noreferrer">
+          <button>Zego</button>
+        </a>
+        <a href="https://sockets-production-4c05.up.railway.app/action.html" target="_blank" rel="noopener noreferrer">
+          <button>Sockets</button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
